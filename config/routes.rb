4399633df050a2e5e5c24fe980545d81ea97cf1f
Stephen Touset <stephen@touset.org>
2010-10-05 10:27:58 -0400

    Render javascript expansions automatically

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  match '/javascripts/*path' => 'asset_bundler/javascripts#show', :defaults => { :formats => :js }
+  match '/javascripts/expansions/*path.:format' => 'asset_bundler/javascripts#index'
+  match '/javascripts/*path.:format'            => 'asset_bundler/javascripts#show'
+  
+  match '/stylesheets/expansions/*path.:format' => 'asset_bundler/stylesheets#index'
+  match '/stylesheets/*path.:format'            => 'asset_bundler/stylesheets#show'
 end
